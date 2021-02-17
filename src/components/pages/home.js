c59ce381c3ddc0b277962577ca07759a5af57013
Daniel Gosselin <dangosscode@gmail.com>
2021-02-17 14:01:15 -0700

    Overview of the Link Component in React

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,12 @@
 import React from "react"
+import PortfolioContainer from "../portfolio/portfolio-container"
 
 export default function() {
   return (
     <div>
-      Homepage
+      <h1>Homepage</h1>
+      <PortfolioContainer />
+
     </div>
   )
 }
