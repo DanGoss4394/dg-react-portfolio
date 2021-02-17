17b3f626c7ddd6a59c600261677554f37de65aa3
Daniel Gosselin <dangosscode@gmail.com>
2021-02-17 11:31:57 -0700

    Introduction to React Conditionals

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,6 +10,7 @@ export default class PortfolioContainer extends Component {
 
     this.state = {
       pageTitle: "Welcome to my portfolio",
+      isLoading: true,
       data: [
         { title: "Quip", catagory: "eCommerce" },
         { title: "Eventbrite", catagory: "Scheduling" },
@@ -35,6 +36,9 @@ export default class PortfolioContainer extends Component {
   }
 
   render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
