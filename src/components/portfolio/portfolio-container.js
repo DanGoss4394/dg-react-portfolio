c08a5dd982b46bc34ee04efada776b0064621595
Daniel Gosselin <dangosscode@gmail.com>
2021-02-18 09:55:05 -0700

    How to Render API Data in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -12,20 +12,10 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: [
-        { title: "Quip", catagory: "eCommerce", slug: "quip" },
-        { title: "Eventbrite", catagory: "Scheduling", slug: "eventbrite" },
-        {
-          title: "Ministry Safe",
-          catagory: "Enterprise",
-          slug: "mintstry-safe",
-        },
-        { title: "SwingAway", catagory: "eCommerce", slug: "swingaway" },
-      ],
+      data: [],
     };
 
     this.handleFilter = this.handleFilter.bind(this);
-    this.getPortfolioItems = this.getPortfolioItems.bind(this);
   }
 
   handleFilter(filter) {
@@ -40,7 +30,9 @@ export default class PortfolioContainer extends Component {
     axios
       .get("https://dsgoose.devcamp.space/portfolio/portfolio_items")
       .then((response) => {
-        console.log("response data", response);
+        this.setState({
+          data: response.data.portfolio_items,
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -49,17 +41,18 @@ export default class PortfolioContainer extends Component {
 
   portfolioItems() {
     return this.state.data.map((item) => {
-      return (
-        <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />
-      );
+      return <PortfolioItem title={item.name} url={item.url} slug={item.id} />;
     });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
   }
 
   render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
-    this.getPortfolioItems();
 
     return (
       <div>
