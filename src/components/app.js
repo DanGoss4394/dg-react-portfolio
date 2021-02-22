22226c7dfa25341e757de7e80101d11e1c76543f
Daniel Gosselin <dangosscode@gmail.com>
2021-02-22 15:29:55 -0700

    Introduction to Render Props in React

--- src/components/app.js
+++ src/components/app.js
@@ -7,21 +7,54 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import NoMatch from "./pages/no-match";
-import Auth from "./pages/auth"
+import Auth from "./pages/auth";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this)
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this)
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+    });
+  }
+
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <Router>
           <div>
-
             <NavigationContainer />
             <Switch>
               <Route exact path="/" component={Home} />
+
               <Route path="/about-me" component={About} />
-              <Route path="/auth" component={Auth} />
+
+              <Route
+                path="/auth"
+                render={(props) => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handlesUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                  />
+                )}
+              />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
               <Route
