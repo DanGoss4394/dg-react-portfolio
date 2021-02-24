d44e56406d6e728500857e148787e1b79fe4841e
Daniel Gosselin <dangoscode@gmail.com>
2021-02-24 13:26:00 -0700

    Creating Portfolio Items from React Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioForm extends Component {
   constructor(props) {
@@ -21,17 +22,28 @@ export default class PortfolioForm extends Component {
   buildForm() {
     let formData = new FormData();
 
-    formData.append("protfolio_item[name]", this.state.name);
-    formData.append("protfolio_item[description]", this.state.description);
-    formData.append("protfolio_item[url]", this.state.url);
-    formData.append("protfolio_item[category]", this.state.category);
-    formData.append("protfolio_item[position]", this.state.position);
-    
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.position);
+
     return formData;
   }
 
   handleSubmit(event) {
-    this.buildForm();
+    axios
+      .post(
+        "https://dsgoose.devcamp.space/portfolio/portfolio_items",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("portfolio from handleSubmit error", error);
+      });
     event.preventDefault();
   }
 
