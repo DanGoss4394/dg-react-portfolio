d04389472844ab20e30604094a19c0e40b0f5a38
Daniel Gosselin <dangoscode@gmail.com>
2021-02-25 14:25:57 -0700

    How to Style Font Awesome Icons in React

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
 const PortfolioSidebarList = (props) => {
   const portfolioList = props.data.map((portfolioItem) => {
     return (
@@ -9,11 +8,12 @@ const PortfolioSidebarList = (props) => {
         <div className="portfolio-item-thumb-img">
           <img src={portfolioItem.thumb_image_url} />
         </div>
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        <a onClick={() => props.handleDeleteClick(portfolioItem)}>
-        <FontAwesomeIcon icon="trash" />
-        </a>
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
+          <a className="delete-icon" onClick={() => props.handleDeleteClick(portfolioItem)}>
+            <FontAwesomeIcon icon="trash" />
+          </a>
+        </div>
       </div>
     );
   });
