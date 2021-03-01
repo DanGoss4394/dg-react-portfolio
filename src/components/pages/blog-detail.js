d1e6cc197351d114874e2297d0179556ad9d346f
Daniel Gosselin <dangoscode@gmail.com>
2021-03-01 15:35:21 -0700

    Calling a Single Blog Item from the API in React

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+
 class BlogDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {},
+    };
   }
-  render() { 
-    return ( 
+
+  getBlogItem() {
+    axios
+      .get(
+        `https://dsgoose.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+      )
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
+  render() {
+    return (
       <div>
         <h1>Blog Detail</h1>
       </div>
-     );
+    );
   }
 }
- 
+
 export default BlogDetail;
