a90aee461c332c733afcb9c74b5b1aad66def76d
Daniel Gosselin <dangoscode@gmail.com>
2021-03-04 13:28:38 -0700

    Adding Click Handler to Render the BlogForm Component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
 
+import BlogForm from "../blog/blog-form";
 import BlogFeaturedImage from "../blog/blog-featured-image";
 
 class BlogDetail extends Component {
@@ -10,7 +11,14 @@ class BlogDetail extends Component {
     this.state = {
       currentId: this.props.match.params.slug,
       blogItem: {},
+      editMode: false,
     };
+
+    this.handleEditClick = this.handleEditClick.bind(this);
+  }
+
+  handleEditClick() {
+    this.setState({ editMode: true });
   }
 
   getBlogItem() {
@@ -39,15 +47,21 @@ class BlogDetail extends Component {
       featured_image_url,
       blog_status,
     } = this.state.blogItem;
-    return (
-      <div className="blog-container">
-        <div className="content-container">
-          <h1>{title}</h1>
-          <BlogFeaturedImage img={featured_image_url} />
-          <div className="content">{ReactHtmlParser(content)}</div>
-        </div>
-      </div>
-    );
+
+    const contentManager = () => {
+      if (this.state.editMode) {
+        return <BlogForm />;
+      } else {
+        return (
+          <div className="content-container">
+            <h1 onClick={this.handleEditClick}>{title}</h1>
+            <BlogFeaturedImage img={featured_image_url} />
+            <div className="content">{ReactHtmlParser(content)}</div>
+          </div>
+        );
+      }
+    };
+    return <div className="blog-container">{contentManager()}</div>;
   }
 }
 
