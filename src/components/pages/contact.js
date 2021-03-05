69eb14eaf67923064642e3bf241dc64d6dfa5aad
Daniel Gosselin <dangoscode@gmail.com>
2021-03-05 11:01:45 -0700

    Building Contact Page Styles for the React Portfolio

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,40 @@
-import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
 
-export default function() {
+export default function () {
   return (
-    <div>
-      Contact Me
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+            <div className="text">970-481-6191</div>
+          </div>
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+            <div className="text">dangoscode@gmail.com</div>
+          </div>
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+            <div className="text">Wiggins, CO</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
