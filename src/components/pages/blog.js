1fa6604be068bdca80ec5d07a725fea52dbacd1f
Daniel Gosselin <dangoscode@gmail.com>
2021-03-05 14:17:09 -0700

    Enabling the Ability to Delete Blog Posts

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -25,6 +25,26 @@ export default class Blog extends Component {
     this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(
       this
     );
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleDeleteClick(blog) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.setState({
+          blogItems: this.state.blogItems.filter((blogItem) => {
+            return blog.id !== blogItem.id;
+          }),
+        });
+        return response.data;
+      })
+      .catch((error) => {
+        console / log("deleted blog error", error);
+      });
   }
 
   handleSuccessfulNewBlogSubmission(blog) {
@@ -96,7 +116,18 @@ export default class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map((blogItem) => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      if (this.props.loggedInStatus === "LOGGED_IN") {
+        return (
+          <div className="admin-blog-wrapper" key={blogItem.id}>
+            <BlogItem blogItem={blogItem} />
+            <a onClick={() => this.handleDeleteClick(blogItem)}>
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
+        );
+      } else {
+        return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      }
     });
 
     return (
