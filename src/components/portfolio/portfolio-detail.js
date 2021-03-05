9474acc50f1cac97efd420807d4c95631e261a7d
Daniel Gosselin <dangoscode@gmail.com>
2021-03-05 11:33:18 -0700

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { Component } from "react";
 
-export default function (props) {
-  return (
-    <div>
-      <h2>Portfolio Details for {props.match.params.slug}</h2>
-    </div>
-  );
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://dsgoose.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log("res", response);
+      })
+      .catch((error) => {
+        console.log("getportfolioitem error", error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Details for {this.props.match.params.slug}</h2>
+      </div>
+    );
+  }
 }
